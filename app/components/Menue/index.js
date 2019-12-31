bdb2e2d860aa6612510700e18c2c8dbbe7874419
kamel allababidi <kamel@repocoin.io>
2019-12-31 11:27:56 +0400

    add menue to responsive design

--- app/components/Menue/index.js
+++ app/components/Menue/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import './_header.css';
-import { useMediaQuery } from 'react-responsive'
-import logo from '../../images/logo.png';
-import avatar from '../../images/ic_avatar.png'
+import './_menue.css';
 import men1 from '../../images/ic_menu1.svg'
 import men1active from '../../images/ic_menu1_active.svg'
 import men2 from '../../images/ic_menu2.svg'
@@ -11,48 +8,23 @@ import men3 from '../../images/ic_menu3.svg'
 import men3active from '../../images/ic_menu3_active.svg'
 import men4 from '../../images/ic_menu4.svg'
 import men4active from '../../images/ic_menu4_active.svg'
-import alert from '../../images/ic_alert.svg'
-import menue from '../../images/ic_menu.svg'
-import close from '../../images/ic_close.svg'
-import search from '../../images/ic_search.svg'
+import men5 from '../../images/ic_logout.svg'
+import men5active from '../../images/ic_logout_active.svg'
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 
-const Desktop = ({ children }) => {
-  const isDesktop = useMediaQuery({ minWidth: 992 })
-  return isDesktop ? children : null
-}
-const Tablet = ({ children }) => {
-  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 991 })
-  return isTablet ? children : null
-}
-const Mobile = ({ children }) => {
-  const isMobile = useMediaQuery({ maxWidth: 767 })
-  return isMobile ? children : null
-}
-
-function Header(props) {
-  const classes = useStyles();
-  const [selectedIndex, setSelectedIndex] = React.useState(0);
-  const [open, setOpen] = React.useState(false)
-
-  const handleListItemClick = (event, index) => {
-    setSelectedIndex(index);
-  };
-
+function Menue() {
+    const classes = useStyles();
+    const [selectedIndex, setSelectedIndex] = React.useState(0);
+  
+    const handleListItemClick = (event, index) => {
+      setSelectedIndex(index);
+    };
   return (
-  <div>
-    <Desktop>
-      <div className='header-desktop'>
-        <img style={{width: 130}} src={logo} />
-        <div className='avatar-container'>
-          <img className='avatar' src={avatar} />
-          <p className='avatar-text'>الاسم</p>
-        </div>
-        <div>
+    <div className='menue-container'>
         <List component="nav" aria-label="contacts">
           <ListItem className={classes.listItemContainer} button
             classes={{
@@ -102,75 +74,60 @@ function Header(props) {
               <img className='icon' src={selectedIndex === 3 ? men4active: men4} />
             </ListItemIcon>
           </ListItem>
+          <ListItem className={classes.listItemContainer} button
+            classes={{
+              selected: classes.selected
+            }}
+            selected={selectedIndex === 4}
+            onClick={event => handleListItemClick(event, 4)}
+          >
+            <ListItemText className={classes.listText} classes={{ secondary: selectedIndex === 4? classes.whiteColor: classes.grayColor }} secondary="عنوان خامس"/>
+            <ListItemIcon className={classes.iconContainer}>
+              <img className='icon' src={selectedIndex === 4 ? men5active: men5} />
+            </ListItemIcon>
+          </ListItem>
         </List>
-        </div>
-
-      </div>
-    </Desktop>
-    <Tablet>
-      <div className='header-tablet'>
-        <img src={alert} className='alert' />
-        <img src={logo}  className='logo-big'/>
-        <img src={open? close: menue} className='menue' onClick={() => {
-          setOpen(!open)
-          props.toggleMenue()
-        }} />
-      </div>
-    </Tablet>
-    <Mobile>
-      <div className='header-mobile-container'>
-        <div className='header-mobile'>
-          <img src={alert} className='alert-small' />
-          <img src={logo} className='logo-small'/>
-          <img src={open? close: menue} className='menue-small' onClick={() => {
-            setOpen(!open)
-            props.toggleMenue()
-          }} />
-        </div>
-        <div className='search-container'>
-          <img src={search} className='search' />
-          <p className='search-text'>بحث</p>
-        </div>
-      </div>
-    </Mobile>
-  </div>
+    </div>
   );
 }
 
-export default Header;
-
+export default Menue;
 
 
 const useStyles = makeStyles({
-  listItemContainer: {
-    height: 40,
-    paddingRight: 0,
-    marginRight: 0,
-    '&$selected': {
-      backgroundColor: '#03a1fc'
+    listItemContainer: {
+      height: 40,
+      width: '97%',
+      paddingRight: 20,
+      marginRight: 30,
+      '&$selected': {
+        backgroundColor: '#03a1fc'
+      },
+      borderRadius: 3,
+      marginTop:10,
+      marginLeft: 10,
+      marginRight: 10,
     },
-    borderRadius: 3
-  },
-  listText: {
-    textAlign: 'right',
-    color: 'red',
-  },
-  iconContainer: {
-    justifyContent: 'flex-end',
-  },
-  icon: {
-    width: 10,
-  },
-  highlight: {
-    backgroundColor: 'red',
-  },
-  selected: {
-    background: "#03a1fc"
-  },
-  whiteColor: {
-    color: '#ffffff'
-  },
-  grayColor: {
-    color: '#8f8f8f'
-  },
-});
+    listText: {
+      textAlign: 'right',
+      color: 'red',
+    },
+    iconContainer: {
+      justifyContent: 'flex-end',
+    },
+    icon: {
+      width: 10,
+    },
+    highlight: {
+      backgroundColor: 'red',
+    },
+    selected: {
+      background: "#03a1fc"
+    },
+    whiteColor: {
+      color: '#ffffff',
+    },
+    grayColor: {
+      color: '#8f8f8f'
+    },
+  });
--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,24 +1,38 @@
-import React, { useEffect, memo } from 'react';
+import React, { useEffect, memo, useState } from 'react';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { Container, Row, Col } from 'react-bootstrap';
 import Header from 'components/Header';
 import SubHeader from 'components/SubHeader';
+import Menue from 'components/Menue';
 import ColoredLine from 'components/ColoredLine';
 import SearchBar from 'components/SearchBar';
 import Content from 'components/Content';
 import './_home.css';
 
 export function HomePage() {
+  const [menue, setmenue] = useState(false);
+  const toggleMenue = () => {
+    setmenue(!menue)
+  }
   return (
     <Container className='container' fluid={true}>
-      <Header/>
-      <div className='content-container'>
-        <ColoredLine/>
-        <SubHeader/>
-        <SearchBar/>
-        <Content/>
+      <Header toggleMenue={toggleMenue}/>
+      {
+        menue ?
+        <div className='content-container'>
+          <ColoredLine/>
+          <Menue/>
+        </div>
+        :
+        <div className='content-container'>
+          <ColoredLine/>
+          <SubHeader/>
+          <SearchBar/>
+          <Content/>
       </div>
+      }
+      
     </Container>
   );
 }
